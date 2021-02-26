71d2724cd141ca2d2c845d6ec7ee5faacd3fb95d
Steven <sastudillo6@gmail.com>
2021-02-26 14:11:41 -0500

    Initialized rails server

---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+# seed songs database.
+Song.create([
+  { title: "Boom", author: "Gucci Mane", time: "3:00"},
+  { title: "Time Travel Kool Aid", author: "Half An Orange", time: "3:10"},
+  { title: "Dragostea Din Tei", author: "O-Zone", time: "2:47" }
+]);
